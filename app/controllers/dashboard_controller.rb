ee2ba8f6a1770ab14427472f1f3611a5be65a421
Josh Mobley <josh@joshmobley.net>
2018-08-13 14:22:30 -0400

    adding ability to create shouts

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,8 @@
 class DashboardController < ApplicationController
 
     def show
-        render
+        @shout = Shout.new
+        @shouts = current_user.shouts
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   root to: "home#show"
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, only: [:create]
+  resource :shouts, only: [:create]
 
   resources :users, only: [:create] do
     resource :password,
